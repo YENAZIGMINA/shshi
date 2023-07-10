d1d6ead614a37b0319585174800c3a1974285b7f
lee_yena <leeyena4724@naver.com>
2023-07-10 13:15:38 +0900

    7/10

--- main.js
+++ main.js
@@ -74,16 +74,70 @@ window.addEventListener("scroll",scrollHeader)
 
 
 //Reveal animation (사이트 참고)
-ScrollReveal().reveal('.home_img, .about_date, .about_img', {
+ScrollReveal().reveal('.home_img, .about_date, .about_img, .recently_data, .recently_img, popular_card, .footer_description, .footer_content, .footer_info', {
     origin:'top', 
     distance:'60px', 
     duration: 2500, 
     delay:400 ,
-    reset: true
+    //reset: true
 });
 
 
 
 ScrollReveal().reveal('.home_img', {origin:'bottom'});
-ScrollReveal().reveal('.about_date', {origin:'left'});
-ScrollReveal().reveal('.about_img', {origin:'right'});
+ScrollReveal().reveal('.about_date, .recently_data', {origin:'left'});
+ScrollReveal().reveal('.about_img, .recently_img', {origin:'right'});
+ScrollReveal().reveal('.popular_card', { interval: 100 });
+
+
+
+/* scroll-up */
+/* window.addEventListener("scroll",function(){
+    scrollup()
+}) */
+
+let scrollup=()=>{
+    let scollup=document.getElementById('scrollup');
+    pageYOffset>=350?scollup.classList.add('show-scroll'):scollup.classList.remove('show-scroll');
+}
+window.addEventListener("scroll",scrollup)
+
+
+
+//menu
+let scrollActive=()=>{
+    let scrollY=pageYOffset; //스크롤내리면 제일 상단부터 지나온 페이지 높이값
+    //console.log(scrollY)
+
+    let sections=document.querySelectorAll('section[id]');
+    //section의 속성중 id를 가지고 있는 section들 모두
+    //console.log(sections)
+
+    //sections.forEach((아이템, 아이템의 인덱스번호)=>{}) //sections의 아이템 각각에게 할 일을 지정
+                                                        //매개변수 두개 받을 수 있음  
+                                                        //인덱스번호는 생략가능 //할일 끝나면 종료
+    sections.forEach((current)=>{
+        let sectionHeight=current.offsetHeight; //자신 영역의 전체 높이값
+        //console.log(sectionHeight)
+        let sectionTop=current.offsetTop - 100; 
+        //자신의 영역이 시작되는 지점(=내 머리가 천장에 닿는 지점)
+        //값을 빼는 이유는 조금 더 빠른 시점에서 색상 변하도록 하려고
+
+        let sectionId=current.getAttribute('id'); //id값 뽑는 방법
+        //console.log(sectionId)
+
+        //herf 중에 sectionId를 포함한 것 뽑기
+        let sectionClass=document.querySelector(`.nav_menu a[href*="${sectionId}"]`);
+        console.log(sectionClass)
+
+        if(scrollY>sectionTop && scrollY<=sectionTop + sectionHeight){
+            //지나온 페이지 높이값이 내 영역의 머리가 닿는 지점보다 더 클때 +  
+            //지나온 페이지 높이값이 내머리가 닿는지점+내영역의 높이보다 더 작을 때
+            sectionClass.classList.add('active-link');
+        }else{
+            sectionClass.classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener("scroll",scrollActive)
